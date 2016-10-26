d62b17d443ce5e2649dc38f363d18481b5f8fbaa
claudia0 <reyesclaudiav@gmail.com>
2016-10-26 13:33:07 -0500

    logre crear un div js, y usar insertBeforegit add .

--- Trello/js/main.js
+++ Trello/js/main.js
@@ -1,14 +1,15 @@
 
-var botonLista = document.getElementById("lista");
-botonLista.addEventListener("click",añadirLista);
-var contenidoDiv = document.getElementById("caja");
+var botonLista = document.getElementById("lista"); //boton de inicio creado en html//
+botonLista.addEventListener("click",añadirLista); //la indicacion addEventListener va afuera de  la funcion (que haga click al boton se activa la funcion)//
+var contenidoDiv = document.getElementById("caja") //el div que contiene el boton anterior
 
 function añadirLista(){
+
 	var input = document.createElement("input");
 	input.setAttribute("placeholder","Añadir Título...");
-	var boton = document.createElement("button");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+	var boton = document.createElement("button"); 
+	boton.setAttribute("type", "button");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 	boton.setAttribute("class", "btn btn-primary");
-	boton.setAttribute("type", "button");
 	boton.textContent = "Guardar";
 	contenidoDiv.appendChild(input);
 	contenidoDiv.appendChild(boton);
@@ -16,33 +17,36 @@ function añadirLista(){
 	boton.addEventListener("click",botonGuardar);
 
 	function botonGuardar(){
-		var div2 = document.getElementById("caja2");
+		var rowpadre = document.getElementById("rowpapa")
+		var divQueseRecorre = document.createElement("div");
+		divQueseRecorre.setAttribute("class", "col-xs-12 col-lg-4 col-md-4");
+		var divinternoQueRecorre = document.createElement("div");
+		divinternoQueRecorre.setAttribute("class","col-xs-12 col-lg-12 col-md-12");
+		var tituloTablero = document.createElement("h4");
 		var textArea = document.createElement("textarea");
 		textArea.setAttribute("placeholder","Añadir Tarjeta");
 		textArea.style.display = "block";
 		var botonAddLista = document.createElement("button");
 		botonAddLista.setAttribute("type", "button");
 		botonAddLista.setAttribute("class", "btn btn-primary");
-		botonAddLista.textContent = "Añadir Lista";
-		var tituloTablero = document.createElement("h4");
+		botonAddLista.textContent = "Añadir Tarjeta";
 		tituloTablero.innerHTML = input.value;
-		div2.appendChild(tituloTablero);
-		div2.appendChild(textArea);
-		div2.appendChild(botonAddLista);
+		divQueseRecorre.appendChild(tituloTablero);
+		divQueseRecorre.appendChild(divinternoQueRecorre);
+		divinternoQueRecorre.appendChild(textArea);
+		divinternoQueRecorre.appendChild(botonAddLista);
+		rowpadre.insertBefore(divQueseRecorre,rowpadre.firstChild)
+
 		
-
-
 	 botonAddLista.addEventListener("click",miLista);
 	
     function miLista(){
- 		var div3 = document.getElementById("caja3");
+    	
+ 		
  	    var p = document.createElement("p");
- 	    var inputLista = document.createElement("input");
-		inputLista.setAttribute("placeholder","Añadir Lista");
- 	    div3.appendChild(p);
- 	    div3.appendChild(inputLista);
- 	    p.innerHTML = inputLista.value;
-
+ 	    p.innerHTML = textArea.value;
+ 	    divQueseRecorre.appendChild(p);
+ 	    textArea.value = "";
 	    
      	}
 	}
