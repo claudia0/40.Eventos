a57b03c7173f7d209b23517880384f1fdb373cb0
claudia0 <reyesclaudiav@gmail.com>
2016-10-27 00:30:45 -0500

    cambio de boton por input de inicio y un poquitou de estilosgit add .

--- Trello/js/main.js
+++ Trello/js/main.js
@@ -1,25 +1,25 @@
 
-var botonLista = document.getElementById("lista"); //boton de inicio creado en html//
-botonLista.addEventListener("click",añadirLista); //la indicacion addEventListener va afuera de  la funcion (que haga click al boton se activa la funcion)//
+var inputLista = document.getElementById("AddLista"); //boton de inicio creado en html//
+inputLista.addEventListener("click",añadirLista); //la indicacion addEventListener afuera de  la funcion (click al boton activa funcion)//
 var contenidoDiv = document.getElementById("caja") //el div que contiene el boton anterior
 
 function añadirLista(){
 
-	var input = document.createElement("input");
-	input.setAttribute("placeholder","Añadir Título...");
 	var boton = document.createElement("button"); 
 	boton.setAttribute("type", "button");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 	boton.setAttribute("class", "btn btn-primary");
 	boton.textContent = "Guardar";
-	contenidoDiv.appendChild(input);
 	contenidoDiv.appendChild(boton);
-		
+	inputLista.value = "";
+
 	boton.addEventListener("click",botonGuardar);
 
 	function botonGuardar(){
-		var rowpadre = document.getElementById("rowpapa")
+		var rowpadre = document.getElementById("rowpapa")//Obtener el id del row que es padre de los divs a utilizar//
 		var divQueseRecorre = document.createElement("div");
 		divQueseRecorre.setAttribute("class", "col-xs-12 col-lg-4 col-md-4");
+		divQueseRecorre.style.backgroundColor = "#5D4E4E";
+		divQueseRecorre.style.marginRight = "1%";
 		var divinternoQueRecorre = document.createElement("div");
 		divinternoQueRecorre.setAttribute("class","col-xs-12 col-lg-12 col-md-12");
 		var tituloTablero = document.createElement("h4");
@@ -30,7 +30,7 @@ function añadirLista(){
 		botonAddLista.setAttribute("type", "button");
 		botonAddLista.setAttribute("class", "btn btn-primary");
 		botonAddLista.textContent = "Añadir Tarjeta";
-		tituloTablero.innerHTML = input.value;
+		tituloTablero.innerHTML = inputLista.value;
 		divQueseRecorre.appendChild(tituloTablero);
 		divQueseRecorre.appendChild(divinternoQueRecorre);
 		divinternoQueRecorre.appendChild(textArea);
